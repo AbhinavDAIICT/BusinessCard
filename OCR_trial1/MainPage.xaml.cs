4bc6caa53e7d6045de057d1ad84b03c23e455a18
AbhinavDAIICT <abhinavtripathi01@hotmail.com>
2014-09-18 13:01:56 +0530

    Added WriteToFile Task to write OCR text to a file
    WriteToFile Task takes an string argument which is the recognized text
    in our case and then it is saved in a local file

--- OCR_trial1/MainPage.xaml.cs
+++ OCR_trial1/MainPage.xaml.cs
@@ -101,6 +101,27 @@ namespace OCR_trial1
                 }
             }
 
+        private async Task WriteToFile(string octext)
+        {
+            // Get the text data from the textbox. 
+            byte[] fileBytes = System.Text.Encoding.UTF8.GetBytes(octext);
+
+            // Get the local folder.
+            StorageFolder local = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+            // Create a new folder name DataFolder.
+            var dataFolder = await local.CreateFolderAsync("DataFolder",CreationCollisionOption.OpenIfExists);
+
+            // Create a new file named DataFile.txt.
+            var file = await dataFolder.CreateFileAsync("DataFile.txt",CreationCollisionOption.ReplaceExisting);
+
+            // Write the data from the textbox.
+            using (var s = await file.OpenStreamForWriteAsync())
+            {
+                s.Write(fileBytes, 0, fileBytes.Length);
+            }
+        }
+
 
         //Code for initialization, capture completed, image availability events; also setting the source for the viewfinder.
         protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
